a79f9508f610cfc09cbdac3adf1c491aecde48d6
Junkoma2 <198304619+Junkoma2@users.noreply.github.com>
2026-06-09 09:36:40 +0900

    fix: sw.jsがAPP_VERSIONからCACHE_NAMEを自動生成するよう修正 (#80)

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'gather-20260607-01'
+importScripts('./version.js')
+
+const CACHE_NAME = 'gather-' + (self.APP_VERSION || 'dev')
 const ASSETS = ['./', './index.html', './styles.css', './version.js', './script.js']
 
 self.addEventListener('install', event => {
